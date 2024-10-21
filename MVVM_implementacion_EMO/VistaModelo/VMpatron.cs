98a5fa75a495b1bce79f68f0bffb8b4917c52f4b
Emiliano Monge <mongee587@gmail.com>
2024-10-21 14:50:55 -0700

    Agregar archivos de proyecto.

---
+++ MVVM_implementacion_EMO/VistaModelo/VMpatron.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_implementacion_EMO.VistaModelo
+{
+    internal class VMpatron : BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+        #endregion
+        #region CONSTRUCTOR
+
+        #endregion
+        #region OBJETOS
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+
+        #endregion
+        #region PROCESOS
+        public async Task ProcesoAsyncrong()
+        {
+
+        }
+
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+    }
+}
